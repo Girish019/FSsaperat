e13379152f1e19e2c456c35105cacd2b203359f7
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-10-22 17:53:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Chandan:Chandan@cluster0.
 DB_NAME = os.environ.get("DATABASE_NAME", "FSsaperat")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001842556179"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "15"))
 
