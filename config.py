b11f60ac2431651682376e78eb3093c0502d0acf
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-04 13:28:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,21 +8,18 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6102959656:AAGNVH5A8IHdGs8fSb7uoVkOT-FHWVXRgwE")
-# TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6180766269:AAF0jG2fxEc6iu19Ps8HFAQYa3lmQbtyTeg")
-# TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5649086028:AAGZWzb35VoeXH4esUGv4XlMs8JBkHJxwjY")
-
-#Your API ID from my.telegram.org
+#TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5649086028:AAGZWzb35VoeXH4esUGv4XlMs8JBkHJxwjY")# alexa 3
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5906626853:AAFzimXzHM4oV1QCT2xvkadzjvjRti7NMBk")# lokey 2
+#Your API ID from my.telegram.org 
 APP_ID = int(os.environ.get("APP_ID", "10755921"))
 
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "d5e49fd3637cba407f17807d31c77977")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001842556179"))
-# LOG_ID = int(os.environ.get("LOG_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002023092398"))
+LOG_ID = int(os.environ.get("LOG_ID", "-1001657067333"))
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5636224141"))
 
@@ -31,18 +28,18 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Chandan:Chandan@cluster0.2lauy.mongodb.net/cluster0?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "FSsaperat")
+DB_NAME = os.environ.get("DATABASE_NAME", "TKHfsbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001842556179"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002023092398"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "15"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1269341939").split()):
+    for x in (os.environ.get("ADMINS", "5678225652 1269341939").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -51,10 +48,10 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "📁𝐅𝐢𝐥𝐞 𝐍𝐚𝐦𝐞</b> : <code>{filename}</code> \n<b>\n𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐛𝐲 :- <a href='https://t.me/dot_serials'>𝐃𝐎𝐓 𝐒𝐄𝐑𝐈𝐀𝐋𝐒</a>\n</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
@@ -68,11 +65,18 @@ ADMINS.append(1284476297)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
+PROGRESS_BAR = """<b>\n
+╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
+┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ Dᴏɴᴇ : {0}%
+┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ Eᴛᴀ: {4}
+╰━━━━━━━━━━━━━━━➣ </b>"""
 LOG_TEXT_P =  """#𝐍𝐞𝐰𝐔𝐬𝐞𝐫
 
 <b>᚛› 𝐈𝐃 - <code>{}</code></b>
 <b>᚛› 𝐍𝐚𝐦𝐞 - {}</b>
-<b>᚛› 𝐅𝐫𝐨𝐦 -  Ultra Stow 🤪</b>
+<b>᚛› 𝐅𝐫𝐨𝐦 - <a href="https://t.me/Tkh_fsbot">TKH Store 🏪</a></b>
 """
 
 logging.basicConfig(
